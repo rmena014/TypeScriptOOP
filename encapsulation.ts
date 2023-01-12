7b02028fb796cafa478bc3c9fb1d33c953587db7
rmena014 <rmena014@gmail.com>
2023-01-12 09:59:16 -0500

    update

--- encapsulation.ts
+++ encapsulation.ts
@@ -1,7 +1,7 @@
 //Encapsulation is the practice of keeping the implementation details of a class hidden from the outside and only exposing a public interface.
 
 class Person {
-  //Two private fields to store data. 
+  //Two private fields to store data.
   private name: string;
   private age: number;
 
@@ -12,16 +12,16 @@ class Person {
     this.age = age;
   }
 
-  // Returns the current value of the name field
   getName(): string {
+    // Returns the current value of the name field
     return this.name;
   }
 
-  // Returns the current value of the age field
   getAge(): number {
+    // Returns the current value of the age field
     return this.age;
   }
-  //Public methods are interacting with the data
+  //Public interface are interacting with the data
 }
 
 // Create an instance of the class with initial values "Richard Mena" and 21
